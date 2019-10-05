493842d9ea183c468999968436a63a8e27d6f015
hilmynaufal <55622577+hilmynaufal@users.noreply.github.com>
2019-10-05 18:58:35 +0700

    Add files via upload

---
+++ src/pbo2/pert2/pkg10118046/latihan18/kelipatan/PBO2PERT210118046Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pert2.pkg10118046.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : menampilkan perulangan bilangan kelipatan 3.5 sampai sepuluh kali
+ */
+public class PBO2PERT210118046Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i<=35;i+=3.5) System.out.println(i);
+        System.out.println("Developed by: M. Hilmy Naufal");
+    }
+
+}
